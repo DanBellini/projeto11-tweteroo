b2d53b319c035894983462ac21553ef111728804
Daniel <danielbellinijf21@gmail.com>
2022-08-27 19:21:06 -0300

    Etapa de POST /sign-up concluida

--- tweterro-back/index.js
+++ tweterro-back/index.js
@@ -1,11 +1,24 @@
-import express, { application } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 const server = express();
-server.use(cors());
 
-server.get("/", (req, res) =>{
-    res.send("teste");
+server.use(cors());
+server.use(express.json())
+
+const users =[];
+const tweets=[];
+
+server.post("/sign-up", (req, res)=>{
+    const newUser = req.body;
+
+    users.push(newUser)
+    res.send("teste1")
 });
+
+server.get("/tweets", (req, res) =>{
+    res.send("teste3");
+});
+
 
 server.listen(5000, () => console.log("Listening on port 5000"));
